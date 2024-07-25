4164f6ac5b72e558afb16a69b97746df7eaad591
Ojas <akshit2309@gmail.com>
2024-07-25 17:39:34 +0530

    about page, contact us page, and privacy policy

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -3,10 +3,29 @@ import Layout from "../components/Layout/Layout";
 
 const AboutPage = () => {
   return (
-    <Layout title="About us">
-      <h1>AboutPage</h1>
+    <Layout title={"About us - Ecommer app"}>
+      <div className="row contactus " style={{margin: "30px"}}>
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
+            perferendis eius temporibus dicta blanditiis doloremque explicabo
+            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
+            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
+            commodi illum quidem neque tempora nam.
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
 
 export default AboutPage;
+
--- client/src/pages/Policy.jsx
+++ client/src/pages/Policy.jsx
@@ -3,8 +3,28 @@ import Layout from "../components/Layout/Layout";
 
 const Policy = () => {
   return (
-    <Layout title="Privacy Policy">
-      <h1>Policy</h1>
+    <Layout title={"Privacy Policy"}>
+      <div className="row contactus" style={{ margin: "30px" }}>
+        <div className="col-md-6 ">
+          <img
+            src="../../public/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Neque dolorum fuga repellendus distinctio voluptate.</p>
+          <p>Aliquid quod dolore cumque in odio?</p>
+          <p>Expedita minus atque ea! Illum, corporis!</p>
+          <p>Magnam, odio. Itaque, iste? Nobis, ratione.</p>
+          <p>Necessitatibus aperiam aliquid numquam minima eos!</p>
+          <p>Vero officiis reprehenderit animi non illum.</p>
+          <p>Possimus atque reprehenderit consequatur commodi cupiditate.</p>
+          <p>Numquam laudantium a rerum nisi cum.</p>
+          <p>Error at dolorem iusto assumenda molestiae.</p> 
+        </div>
+      </div>
     </Layout>
   );
 };
